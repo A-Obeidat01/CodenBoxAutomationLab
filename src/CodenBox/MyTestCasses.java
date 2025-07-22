ad9610dadf1fa5f591e987618d982d2b5622fb55
abd <abdalrahmanobiedat61@gmail.com>
2025-07-22 19:47:38 +0300

    update

--- src/CodenBox/MyTestCasses.java
+++ src/CodenBox/MyTestCasses.java
@@ -2,6 +2,7 @@ package CodenBox;
 
 import static org.testng.Assert.ARRAY_MISMATCH_TEMPLATE;
 
+import java.awt.Desktop.Action;
 import java.awt.event.WindowEvent;
 import java.net.URI;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -85,8 +87,8 @@ public void RadioButton() throws InterruptedException {
 		driver.switchTo().window(allTabs.get(0));
 	}
 	
-	@Test (priority = 6,enabled = true)
-	public void SwitchToAlert  () throws InterruptedException {
+	@Test (priority = 6,enabled = false)
+	public void SwitchToAlert  ()  {
 		WebElement yourName = driver.findElement(By.id("name"));
 		yourName.sendKeys("abd");
 		driver.findElement(By.id("alertbtn")).click();
@@ -96,6 +98,67 @@ public void RadioButton() throws InterruptedException {
 		
 	}
 	
+
+	@Test (priority = 7,enabled = false)
+	public void Table  ()  {
+		WebElement myTable = driver.findElement(By.id("product"));
+		List<WebElement> data= myTable.findElements(By.tagName("td"));
+		
+		for (int i=0 ; i<data.size();i++) {
+			System.out.println(data.get(i).getText());
+		}
+	}
+	
+	@Test (priority = 8,enabled = false)
+	public void ElementDisplayed  () throws InterruptedException {
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,1500)");
+//Elements
+		WebElement text = driver.findElement(By.id("displayed-text"));
+		WebElement hide = driver.findElement(By.id("hide-textbox"));
+		WebElement show = driver.findElement(By.id("show-textbox"));
+//Action
+		text.sendKeys("abd");
+		Thread.sleep(1000);
+		hide.click();
+		Thread.sleep(1000);
+		show.click();	
+	}
+	
+	@Test (priority = 9,enabled = false)
+	public void EnabledAndDisabled  () throws InterruptedException {
+		
+		driver.findElement(By.id("disabled-button")).click();
+		System.out.println(driver.findElement(By.id("enabled-example-input")).isEnabled());
+		driver.findElement(By.id("enabled-button")).click();
+		System.out.println(driver.findElement(By.id("enabled-example-input")).isEnabled());
+		
+	}
+	
+	@Test (priority = 10,enabled = false)
+	public void MouseHover  () throws InterruptedException {
+		
+		WebElement mousehover = driver.findElement(By.id("mousehover"));
+		Actions action = new Actions(driver);
+		action.moveToElement(mousehover).build().perform();
+		driver.findElement(By.linkText("Top")).click();
+		
+	}
+	
+	@Test(priority = 11,enabled = true)
+	public void Calender() {
+		
+		driver.findElement(By.linkText("Booking Calendar")).click();
+		
+		Set<String> handles = driver.getWindowHandles();
+
+		List<String> allTabs = new ArrayList<>(handles);
+
+		driver.switchTo().window(allTabs.get(1));
+		
+		driver.findElement(By.id("name1")).sendKeys("abd");
+	}
 	
 }
 
